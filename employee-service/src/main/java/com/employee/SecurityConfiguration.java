a59926b8b29e8febe36f3381fcc4be4b80d73559
Marcos Luna <josemarcosluna9@gmail.com>
2023-05-26 16:38:40 -0600

    update employee service for use new keycloak

--- employee-service/src/main/java/com/employee/SecurityConfiguration.java
+++ employee-service/src/main/java/com/employee/SecurityConfiguration.java
@@ -3,7 +3,13 @@ package com.employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import com.employee.services.KeycloakLogoutHandler;
 
 /**
  * Security Configuration Class.
@@ -12,15 +18,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    public SecurityConfiguration(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests(authz -> authz
                         .requestMatchers("/employees/**")
                         .authenticated())
-                .csrf().disable()
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+                .csrf().disable();
 
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/");
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
 }
